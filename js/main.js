14a9c95c7e4aed52feb3969cc730c50487c1c2a7
Peppe2496 <giuseppemusellajob@gmail.com>
2023-11-21 17:39:13 +0100

    finished

--- js/main.js
+++ js/main.js
@@ -8,43 +8,72 @@ let ourTeam =
             name: "Wayne Barnett",
 
             role: "Founder & CEO",
-            picture: "./img/wayne-barnett-founder-ceo.jpg"
+            picture: "wayne-barnett-founder-ceo.jpg"
         },
 
         {
             name: "Angela Caroll",
 
             role: "Chief Editor",
-            picture: "./img/angela-caroll-chief-editor.jpg"
+            picture: "angela-caroll-chief-editor.jpg"
         },
 
         {
             name: "Walter Gordon",
 
             role: "Office Manager",
-            picture: "./img/walter-gordon-office-manager.jpg"
+            picture: "walter-gordon-office-manager.jpg"
         },
 
         {
             name: "Angela Lopez",
 
             role: "Social Media Manager",
-            picture: "./img/angela-lopez-social-media-manager.jpg"
+            picture: "angela-lopez-social-media-manager.jpg"
         },
 
         {
             name: "Scott Estrada",
 
             role: "Developer",
-            picture: "./img/scott-estrada-developer.jpg"
+            picture: "scott-estrada-developer.jpg"
         },
 
         {
             name: "Barbara Ramos",
 
             role: "Graphic Designer",
-            picture: "./img/barbara-ramos-graphic-designer.jpg"
+            picture: "barbara-ramos-graphic-designer.jpg"
         }
     ];
 
 console.log(ourTeam);
+
+for (let i = 0; i < ourTeam.length; i++) {
+    console.log("name:", ourTeam[i].name);
+
+    console.log("role:", ourTeam[i].role);
+    console.log("picture:", ourTeam[i].picture);
+    console.log("");
+}
+
+for (let i = 0; i < ourTeam.length; i++) {
+
+document.getElementById("team_cards").innerHTML+= `
+
+        
+<div class="col-4">
+<div class="card">
+  <img src="img/${ourTeam[i].picture}" alt="">
+  <div class="card-body">
+    <h5 class="card-title">${ourTeam[i].name}</h5>
+    <p class="card-text">${ourTeam[i].role}</p>
+    
+  </div>
+</div>
+
+</div>
+`
+}
+
+
